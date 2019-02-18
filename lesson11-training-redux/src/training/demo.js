6ea824e899cb706f7506176ed4fa638321aaf6a8
Minh Tri Le <minhtri.manchester@gmail.com>
2019-02-18 22:02:58 +1100

    lesson12-redux-seperate

--- lesson11-training-redux/src/training/demo.js
+++ lesson11-training-redux/src/training/demo.js
@@ -1,52 +1,19 @@
 import { createStore } from "redux";
-
-var initialState = {
-    status: false,
-    sort : {
-        by: "name",
-        value: 1
-    }
-};
-
-var myReducer = (state = initialState, action) => {
-    if (action.type === "TOGGLE_STATUS") {
-        state.status = !state.status;
-    }
-
-    if (action.type === "SORT") {
-        var { by, value } = action.sort;
-        var { status } = state;
-
-       return {
-           status: status,
-           sort: {
-               by: by,
-               value: value
-           }
-       }
-    }
-    return state;
-}
+import { status, sort } from "./actions/index";
+import myReducer from "./reducers/index";
 
 const store = createStore(myReducer);
 
 // Toggle status
-var action = {
-    type: "TOGGLE_STATUS"
-};
 
 console.log("Default: ", store.getState());
-store.dispatch(action);
+store.dispatch(status());
 console.log("TOGGLE_STATUS: ", store.getState());
 
 // Sort Z - A by name
-var sortAction = {
-    type: "SORT",
-    sort: {
-        by: "name",
-        value: -1
-    }
-};
 console.log("Default: ", store.getState());
-store.dispatch(sortAction);
+store.dispatch(sort({
+    "by": "name",
+    "value": -1
+}));
 console.log("SORT: ", store.getState());
---
+++ lesson11-training-redux/src/training/reducers/index.js
@@ -0,0 +1,12 @@
+import sort from "./sort";
+import status from "./status";
+import {
+    combineReducers
+} from "redux"
+
+const myReducer = combineReducers({
+    status,
+    sort
+});
+
+export default myReducer;
